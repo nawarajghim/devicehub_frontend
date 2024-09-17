46f2ec76d470a6e060b13b7333c8eacbd76ea67f
Noona <noonaposti@gmail.com>
2024-09-17 15:06:08 +0300

    layout routing

---
+++ src/views/Home.tsx
@@ -0,0 +1,13 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <p>this is home</p>
+          {/* Your layout content here */}
+        </div>
+      );
+};
+
+
+export default Home;
---
+++ src/views/Layout.tsx
@@ -0,0 +1,17 @@
+
+const Layout = () => {
+
+    return (
+  
+      <div>
+        <p>This is layout</p>
+  
+        {/* Your layout content here */}
+  
+      </div>
+  
+    );
+  
+  };
+  
+export default Layout;
