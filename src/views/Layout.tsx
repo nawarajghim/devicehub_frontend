9e311f3b26bb0fb1f3aefe9340a0eabe82bb8e98
Noona <noonaposti@gmail.com>
2024-09-19 20:05:16 +0300

    nav bar icon and bar added

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -9,6 +9,7 @@ const Layout = () => {
     <div className="layout">
       <header>
         <nav className="navit">
+
           <ul >
           <li className="navi">
               <Link to="/login">
@@ -17,7 +18,13 @@ const Layout = () => {
             </li>
           <li className="navi">
               <Link to="/">
-              <p className="navi-P">Home</p>
+              <p className="navi-p-active">
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M10 15V12.5" stroke="#5459EA" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M8.39172 2.35004L2.61672 6.97504C1.96672 7.4917 1.55006 8.58337 1.69172 9.40004L2.80006 16.0334C3.00006 17.2167 4.13339 18.175 5.33339 18.175H14.6667C15.8584 18.175 17.0001 17.2084 17.2001 16.0334L18.3084 9.40004C18.4417 8.58337 18.0251 7.4917 17.3834 6.97504L11.6084 2.35837C10.7167 1.6417 9.27506 1.6417 8.39172 2.35004Z" stroke="#5459EA" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                
+                Home</p>
               </Link>
             </li>
             <li className="navi">
@@ -25,9 +32,10 @@ const Layout = () => {
               <p className="">Devices</p>
               </Link>
             </li>
-
           </ul>
         </nav>
+
+        <div className="navbar"></div>
       
 
       </header>
