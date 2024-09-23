0e2d8cab487d1a092e211937e3e16b4365c7dc63
eemiljka <eemiljka@metropolia.fi>
2024-09-23 14:41:08 +0300

    useFetch(single)Device

--- src/hooks/apiHooks.ts
+++ src/hooks/apiHooks.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 
+/******************Types******************/
+
 type Device = {
     name: string;
     deviceClass: string;
@@ -9,6 +11,8 @@ type Device = {
     location?: string;
     settings?: string;
 }
+
+/******************Device Hooks******************/
 
 // Hook to get all devices
 const useFetchDevices = () => {
@@ -34,4 +38,28 @@ const useFetchDevices = () => {
     return {devices, loading, error};
 }
 
-export {useFetchDevices};
+// Hook to get a single device by name
+const useFetchDevice = (name: string) => {
+    const [device, setDevice] = useState<Device | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchDevice = async () => {
+            setLoading(true);
+            setError(null);
+            try {
+                const response = await axios.get<Device>(`http://localhost:3000/api/v1/devices/${name}`);
+                setDevice(response.data)
+            }   catch {
+                setError('Failed to fetch device');
+            }   finally {
+                setLoading(false);
+            }
+        }
+        fetchDevice();
+    }, [name]);
+    return {device, loading, error};
+}
+
+export {useFetchDevices, useFetchDevice};
