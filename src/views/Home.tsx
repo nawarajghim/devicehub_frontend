d7fe5ed8f49e87563675dfc879cc894801d96598
Noona <noonaposti@gmail.com>
2024-09-25 20:59:40 +0300

    home prototype

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,7 +4,25 @@ const Home = () => {
       <h2 className="h2-title">Device Hub</h2>
       <hr className="line"></hr>
 
-      {/* Your layout content here */}
+      {/* Your Home content here */}
+
+      <div className="home-container">
+      <div className="home-intro"></div>
+      <div className="home-boxes">
+        <div className="box">Box 1</div>
+        <div className="box">Box 2</div>
+        <div className="box">Box 3</div>
+        <div className="box">Box 4</div>
+        <div className="box">Box 5</div>
+        <div className="box">Box 6</div>
+      </div>
+    </div>
+
+
+
+
+
+
     </div>
   );
 };
