53c697e97686d6c59891b37f4f73f6f15cbbae76
eemiljka <eemiljka@metropolia.fi>
2024-10-04 11:23:51 +0300

    test

--- src/components/SingleDevice.tsx
+++ src/components/SingleDevice.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useFetchDevice } from "../hooks/apiHooks";
 import { useParams } from "react-router-dom";
+import Data from "../views/data";
 
 const SingleDevice: React.FC = () => {
   const { deviceName } = useParams<{ deviceName: string }>();
@@ -10,20 +11,26 @@ const SingleDevice: React.FC = () => {
   if (error) return <div>{error}</div>;
 
   return (
-    <div>
-      <h2>Device</h2>
-      <ul>
-        <li>
-          <h2>{device?.name}</h2>
-        </li>
-        <li>
-          <p>Class: {device?.deviceClass}</p>
-        </li>
-        <li>{device?.deviceType}</li>
-        <li>{device?.location}</li>
-        <li>{device?.settings}</li>
-      </ul>
-    </div>
+    <>
+      <div>
+        <h1>Device</h1>
+        <ul>
+          <li>
+            <h2>{device?.name}</h2>
+          </li>
+          <li>
+            <p>Class: {device?.deviceClass}</p>
+          </li>
+          <li>{device?.deviceType}</li>
+          <li>{device?.location}</li>
+          <li>{device?.settings}</li>
+        </ul>
+      </div>
+      <div>
+        <h2>Data</h2>
+        <Data />
+      </div>
+    </>
   );
 };
 
