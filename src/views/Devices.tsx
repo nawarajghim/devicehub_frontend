74a9ca68a67d630a6c893eb537cd91364e014f67
Iaroslava Goncharova <111697910+iaroslavagoncharova@users.noreply.github.com>
2024-10-07 21:38:23 +0300

    devices are now being displayed from the database, UI fixed

--- src/views/Devices.tsx
+++ src/views/Devices.tsx
@@ -1,53 +1,50 @@
+import {Link} from "react-router-dom";
 import DeviceClassList from "../components/DeviceClasses";
 import DeviceDataList from "../components/DeviceData";
-import DeviceList from "../components/DeviceList";
+import { useFetchDevices } from "../hooks/apiHooks";
 
 const Devices = () => {
+  const { devices, loading, error } = useFetchDevices();
+  console.log(devices);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+
   return (
     <>
-    <div>
-       <h2 className="h2-title">Device Hub</h2>
-       <hr className="line"></hr>
-       <h3 className="device-h3">Devices</h3>
-    <div className="device-container">
-      
-      <div className="device-card device-info">
-        <h4>Device name</h4>
-        <h4 className="card-info">type</h4>
-        <h4 className="card-info">location</h4>
-        <h4 className="card-info">status</h4>
-    </div>
+      <div>
+        <h2 className="h2-title">Device Hub</h2>
+        <hr className="line"></hr>
+        <h3 className="device-h3">Devices</h3>
 
-    {/* device will show here */}
+        <div className="device-container">
+          <div className="device-card device-info">
+            <h4>Device name</h4>
+            <h4 className="card-info">Class</h4>
+            <h4 className="card-info">Type</h4>
+            <h4 className="card-info">Location</h4>
+            <h4 className="card-info">Status</h4>
+          </div>
 
-    <div className="device-card ">
-        <p>Device 1</p>
-        <p>x</p>
-        <p>metropolia</p>
-        <p>active</p>
-        <button className="device-button">open</button>
-    </div>
-    <div className="device-card">
-        <p>Device 2</p>
-        <p>x</p>
-        <p>metropolia</p>
-        <p>active</p>
-        <button className="device-button">open</button>
-    </div>
-    <hr className="line"></hr>
-      <DeviceList />
-      <DeviceClassList />
-      <DeviceDataList />
-    </div>
+          {devices.map((device) => (
+            <div className="device-card" key={device.name}>
+              <p>{device.name}</p>
+              <p>{device.deviceClass}</p>
+              <p>{device.deviceType}</p>
+              <p>{device.location}</p>
+              <p>{device.status}</p>
+              <Link to={`/devices/name/${device.name}`}>
+                <button className="device-button">Open</button>
+              </Link>
+            </div>
+          ))}
 
-
-
-
-
-
-    </div>
+          <hr className="line"></hr>
+          <DeviceClassList />
+          <DeviceDataList />
+        </div>
+      </div>
     </>
-
   );
 };
 
