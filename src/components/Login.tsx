3b466c6cea0e121c6d5dc7dbd08a06a703b8bfc2
Kamilla <KamillaKa@users.noreply.github.com>
2024-10-25 22:14:07 +0300

    Back button done and responsive

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -50,79 +50,85 @@ const Login: React.FC = () => {
 
   return (
     <>
-      <div className="login-container">
-        <div className="login-left"></div>
-        <div className="login-right">
-          <div className="login-form-container">
-            {isLoggedIn ? (
-              <>
-                <h2 className="h2-login">You are already logged in as admin</h2>
-                <div>
-                  <p className="p-login">Change password</p>
-                  <input
-                    type="password"
-                    placeholder="Enter new password"
-                    value={newPassword}
-                    onChange={(e) => setNewPassword(e.target.value)}
-                  />
-                  <input
-                    type="password"
-                    placeholder="Confirm new password"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                  />
-                  <button
-                    className="loginButton"
-                    onClick={handlePasswordChange}
-                  >
-                    Change Password
+      <div className="login-wrapper">
+        <div className="login-container">
+          <div className="login-left">
+          <button className="back-button" onClick={() => navigate("/")}>
+            ⬅ Back
+          </button>
+          </div>
+          <div className="login-right">
+            <div className="login-form-container">
+              {isLoggedIn ? (
+                <>
+                  <h2 className="h2-login">You are already logged in as admin</h2>
+                  <div>
+                    <p className="p-login">Change password</p>
+                    <input
+                      type="password"
+                      placeholder="Enter new password"
+                      value={newPassword}
+                      onChange={(e) => setNewPassword(e.target.value)}
+                    />
+                    <input
+                      type="password"
+                      placeholder="Confirm new password"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                    <button
+                      className="loginButton"
+                      onClick={handlePasswordChange}
+                    >
+                      Change Password
+                    </button>
+                  </div>
+                  <button onClick={() => navigate("/")} className="loginButton">
+                    Go to Dashboard
                   </button>
-                </div>
-                <button onClick={() => navigate("/")} className="loginButton">
-                  Go to Dashboard
-                </button>
-                <button onClick={handleLogout} className="loginButton">
-                  Log Out
-                </button>
-              </>
-            ) : (
-              <>
-                <h2 className="h2-login">Log In to Admin Panel</h2>
-                <p className="p-login">
-                  Enter your Username and password below
-                </p>
-                <form onSubmit={handleSubmit}>
-                  <label htmlFor="username">USERNAME</label>
-                  <input
-                    type="text"
-                    id="username"
-                    placeholder="Enter your username"
-                    required
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-
-                  <label htmlFor="password">PASSWORD</label>
-                  <input
-                    type="password"
-                    id="password"
-                    placeholder="Enter your password"
-                    required
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-
-                  <button
-                    type="submit"
-                    disabled={loading}
-                    className="loginButton"
-                  >
-                    {loading ? <div className="spinner"></div> : "Log In"}
+                  <button onClick={handleLogout} className="loginButton">
+                    Log Out
                   </button>
-                  {error && <p className="error-message">{error}</p>}
-                </form>
-              </>
-            )}
+                </>
+              ) : (
+                <>
+                  <h2 className="h2-login">Log In to Admin Panel</h2>
+                  <p className="p-login">
+                    Enter your Username and password below
+                  </p>
+                  <form onSubmit={handleSubmit}>
+                    <label htmlFor="username">USERNAME</label>
+                    <input
+                      type="text"
+                      id="username"
+                      placeholder="Enter your username"
+                      required
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+
+                    <label htmlFor="password">PASSWORD</label>
+                    <input
+                      type="password"
+                      id="password"
+                      placeholder="Enter your password"
+                      required
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    <button
+                      type="submit"
+                      disabled={loading}
+                      className="loginButton"
+                    >
+                      {loading ? <div className="spinner"></div> : "Log In"}
+                    </button>
+                    {error && <p className="error-message">{error}</p>}
+                  </form>
+                </>
+              )}
+            </div>
           </div>
         </div>
       </div>
