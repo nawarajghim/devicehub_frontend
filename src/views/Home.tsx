cc2c4bc0611b8ce4fd88f30bcae2ac1f9bff378a
Noona <noonaposti@gmail.com>
2024-10-26 13:14:23 +0300

    home stuff

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -7,14 +7,20 @@ const Home = () => {
       {/* Your Home content here */}
 
       <div className="home-container">
-      <div className="home-intro"></div>
+      <div className="home-intro">
+        <h1>Welcome to Device Hub</h1>
+        <p>
+          Here you can find all the devices that are connected to your network.
+          You can easily manage them and keep track of their status.
+        </p>
+      </div>
       <div className="home-boxes">
-        <div className="box">Box 1</div>
-        <div className="box">Box 2</div>
-        <div className="box">Box 3</div>
-        <div className="box">Box 4</div>
-        <div className="box">Box 5</div>
-        <div className="box">Box 6</div>
+        <div className="box">Device 1</div>
+        <div className="box">Device 2</div>
+        <div className="box">Device 3</div>
+        <div className="box">Device 4</div>
+        <div className="box">Device 5</div>
+        <div className="box">Device 6</div>
       </div>
     </div>
 
