d2234beca69f091e17504cbf4c115041a73e7753
Iaroslava Goncharova <111697910+iaroslavagoncharova@users.noreply.github.com>
2024-11-11 01:30:46 +0200

    navbar fixed, logos fixed for big screens

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useFetchDevices } from "../hooks/apiHooks";
 
 const Home = () => {
@@ -24,47 +24,52 @@ const Home = () => {
           <div className="home-text">
             <h1>Welcome to Device Hub</h1>
             <p>
-            The idea of the Device hub project is to develop a control panel that manages IoT devices. 
-            The project is part of the Nokian Garage project. 
-            The project has been made in cooperation with students from Nokia and Metropolia.
+              The idea of the Device hub project is to develop a control panel
+              that manages IoT devices. The project is part of the Nokian Garage
+              project. The project has been made in cooperation with students
+              from Nokia and Metropolia.
             </p>
           </div>
           {/*<img className="home-img" src="/src/assets/nokia-logo.png" alt="logo" />*/}
         </div>
-        <img
-          className="home-img1"
-          src="/src/assets/nokia1.jpg"
-          alt="logonokia"
-        />
-        <img
-          className="home-img2"
-          src="/src/assets/meta.png"
-          alt="logometropolia"
-        />
-        <div className="home-boxes">
-          {devicesToShow.map((device, index) => {
-            const starClass = `star${(index % 4) + 1}`;
-            return (
-              <Link to={`/devices/name/${device.name}`} key={device.name}>
-              <div className="box" key={device.name}>
-                <div className={`home-iconi ${starClass}`}>
-                  <img
-                    className="home-star"
-                    src="/src/assets/vector.png"
-                    alt="star"
-                  />
-                </div>
-                <img
-                  className="home-dots"
-                  src="/src/assets/dotvertical.png"
-                  alt="dots"
-                />
-                <p>{device.name}</p>
-                <p>{device.location}</p>
-              </div>
-              </Link>
-            );
-          })}
+        <div className="right-side">
+          <div className="logos">
+            <img
+              className="home-img2"
+              src="/src/assets/meta.png"
+              alt="logometropolia"
+            />
+            <img
+              className="home-img1"
+              src="/src/assets/nokia1.jpg"
+              alt="logonokia"
+            />
+          </div>
+          <div className="home-boxes">
+            {devicesToShow.map((device, index) => {
+              const starClass = `star${(index % 4) + 1}`;
+              return (
+                <Link to={`/devices/name/${device.name}`} key={device.name}>
+                  <div className="box" key={device.name}>
+                    <div className={`home-iconi ${starClass}`}>
+                      <img
+                        className="home-star"
+                        src="/src/assets/vector.png"
+                        alt="star"
+                      />
+                    </div>
+                    <img
+                      className="home-dots"
+                      src="/src/assets/dotvertical.png"
+                      alt="dots"
+                    />
+                    <p>{device.name}</p>
+                    <p>{device.location}</p>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
